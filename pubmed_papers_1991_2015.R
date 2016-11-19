11225e6f2c5bdb928a715ce34b2062685185f9f8
pathmes <pathmes@gmail.com>
2016-11-19 22:04:17 +0530

    create two graphs for CMB & KLN of pubmed papers 1991 - 2015

---
+++ pubmed_papers_1991_2015.R
@@ -0,0 +1,19 @@
+search_year <- function(year, term){
+  query <- paste(term, "AND (", year, "[PDAT])")
+  entrez_search(db="pubmed", term=query, retmax=0)$count
+}
+
+year <- 1991:2015
+papersK <- sapply(year, search_year, term="university of kelaniya[AFFL]", USE.NAMES=FALSE)
+
+plot(year, papersK, type='b', main="KLN publications")
+
+search_year <- function(year, term){
+  query <- paste(term, "AND (", year, "[PDAT])")
+  entrez_search(db="pubmed", term=query, retmax=0)$count
+}
+
+year <- 1991:2015
+papersC <- sapply(year, search_year, term="university of colombo[AFFL]", USE.NAMES=FALSE)
+
+plot(year, papersC, type='b', main="CMB publications")
